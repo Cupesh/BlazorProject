d72293ea389d9d807dc0f7f623bb62ce8c2edc4b
Cupesh <cupak.martin90@gmail.com>
2021-12-26 16:34:47 +0000

    model redundant duplication fix

--- BlazorProject.Client/Models/Employee.cs
+++ BlazorProject.Client/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,17 @@ namespace BlazorProject.Client.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "First Name has to be between 2 - 20 characters")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Last Name has to be between 2 - 20 characters")]
         public string LastName { get; set; }
+        [Required]
         public DateTime DateOfBirth { get; set; } = new DateTime(1900, 01, 01);
+        [Required]
         public int Position { get; set; }
+        [Required]
         public DateTime DateJoined { get; set; } = DateTime.Now;
         public DateTime Updated { get; set; } = new DateTime(1900, 01, 01);
 
