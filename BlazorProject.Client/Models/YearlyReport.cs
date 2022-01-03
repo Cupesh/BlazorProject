46cf75ac180be8c54f9bd673ea54a1f69a4f8e7d
Cupesh <cupak.martin90@gmail.com>
2022-01-03 23:33:40 +0000

    yearly reports blazor view

---
+++ BlazorProject.Client/Models/YearlyReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Client.Models
+{
+    public class YearlyReport
+    {
+        [Required]
+        public int Year { get; set; }
+
+        public YearlyReport()
+        {
+        }
+
+        public YearlyReport(int year)
+        {
+            Year = year;
+        }
+    }
+}
